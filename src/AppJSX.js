a04e747f9cba7138dda7aff440dbb1f582e1bf93
Mong <>
2023-05-28 23:42:46 +0900

    first commit

---
+++ src/AppJSX.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import List from './components/List';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <List></List>
+      <Form></Form>
+    </>
+  );
+  }
+
+export default App;
